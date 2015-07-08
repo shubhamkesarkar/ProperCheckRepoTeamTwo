effb0895fc89e4c71f9cbae1dae09cca6184f560
shubhamkesarkar <shubhamkesarkar@gmail.com>
2015-07-09 02:50:59 +0530

    committed all

--- Final mini project (Timer Only).py
+++ Final mini project (Timer Only).py
@@ -18,9 +18,7 @@ savedictionary = {}
 
 
 #some global variables 																													#imports all from module git, because exceptions file needs to be imported
-# global number_saves_before_push
 
-# number_saves_before_push = 0
 
 counter = 1
 
