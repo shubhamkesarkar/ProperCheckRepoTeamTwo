c1bc8688d0ce17cde126c3187e836bfdb2a5af87
shubhamkesarkar <shubhamkesarkar@gmail.com>
2015-07-09 01:30:33 +0530

    your repository has been comitted

--- Final mini project (Timer Only).py
+++ Final mini project (Timer Only).py
@@ -22,61 +22,65 @@ class myOpener(sublime_plugin.EventListener):
 
 	def on_post_save(self,view):
 
+		accepted_extensions = settings.get("extensions"))
+		sublime.message_dialog(accepted_extensions)
 
 
-		temp_dir = str(view.file_name())
 
-		#savedictionary.update({temp_dir:0})
-
-		def repo_check(temp_dir):																									#code checks for .git in the folder	
-			try :		
-				global repo								
-				repo = Repo(temp_dir,search_parent_directories=True)
-				#self.view.insert(edit, 0, str(repo))
-			except InvalidGitRepositoryError :																			#exception handled when .git is not found
-					counter = 0
+		# temp_dir = str(view.file_name())
 
 
-		repo_check(temp_dir)													#function call to repo_check
+		# #savedictionary.update({temp_dir:0})
+
+		# def repo_check(temp_dir):																									#code checks for .git in the folder	
+		# 	try :		
+		# 		global repo								
+		# 		repo = Repo(temp_dir,search_parent_directories=True)
+		# 		#self.view.insert(edit, 0, str(repo))
+		# 	except InvalidGitRepositoryError :																			#exception handled when .git is not found
+		# 			counter = 0
+
+
+		# repo_check(temp_dir)													#function call to repo_check
 		
 
-		global counter
-		if counter == 1:
-			#global repo
-			sublime.message_dialog("File is saved")
-			sublime.message_dialog(str(repo.git.status()))
-			#sublime.message_dialog("You have saved the file")
+		# global counter
+		# if counter == 1:
+		# 	#global repo
+		# 	sublime.message_dialog("File is saved")
+		# 	sublime.message_dialog(str(repo.git.status()))
+		# 	#sublime.message_dialog("You have saved the file")
 
 
-			sublime.message_dialog(str(repo.git.add( temp_dir )))
-			sublime.message_dialog(str(repo.git.commit( m ='your repository has been comitted' )))
+		# 	sublime.message_dialog(str(repo.git.add( temp_dir )))
+		# 	sublime.message_dialog(str(repo.git.commit( m ='your repository has been comitted' )))
 
-			#sublime.message_dialog("and now it has been committed")
-			sublime.message_dialog(str(repo.git.status()))
+		# 	#sublime.message_dialog("and now it has been committed")
+		# 	sublime.message_dialog(str(repo.git.status()))
 
 
-			def push_repo():
-				forwd_slash_index = temp_dir.rfind('/', 0, len(temp_dir))   				#finds index of last forward slash
-				new_dir = temp_dir[0:forwd_slash_index]
-				repo = Repo(new_dir)
-				o = repo.remotes.origin
-				o.pull()	
-				o.push()
-				sublime.message_dialog("repository pushed")
+		# 	def push_repo():
+		# 		forwd_slash_index = temp_dir.rfind('/', 0, len(temp_dir))   				#finds index of last forward slash
+		# 		new_dir = temp_dir[0:forwd_slash_index]
+		# 		repo = Repo(new_dir)
+		# 		o = repo.remotes.origin
+		# 		o.pull()	
+		# 		o.push()
+		# 		sublime.message_dialog("repository pushed")
 
 
 			
 
 
-			if(temp_dir in savedictionary):
-				savedictionary[temp_dir] += 1
-				sublime.message_dialog(str(savedictionary[temp_dir]))
-				sublime.message_dialog(str(settings.get("x_savespush")))
-				if(savedictionary[temp_dir] == settings.get("x_savespush")):
-					savedictionary[temp_dir] = 0
-					push_repo()
-			else:
-				 savedictionary[temp_dir] = 1
+		# 	if(temp_dir in savedictionary):
+		# 		savedictionary[temp_dir] += 1
+		# 		sublime.message_dialog(str(savedictionary[temp_dir]))
+		# 		sublime.message_dialog(str(settings.get("x_savespush")))
+		# 		if(savedictionary[temp_dir] == settings.get("x_savespush")):
+		# 			savedictionary[temp_dir] = 0
+		# 			push_repo()
+		# 	else:
+		# 		 savedictionary[temp_dir] = 1
 
 
 			
