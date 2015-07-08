a28fb8b9ab649069d96bf93085b538bd18e412e7
shubhamkesarkar <shubhamkesarkar@gmail.com>
2015-07-08 23:39:10 +0530

    committed all

--- Final mini project (Timer Only).py
+++ Final mini project (Timer Only).py
@@ -12,9 +12,9 @@ from git import *
 settings = sublime.load_settings("ProperCheckRepoTeamTwo.sublime-settings")
 
 
-dsfdsa
+#dsfdsa
 
-savedictionary = {"hello":123}
+savedictionary = {}
 
 
 
@@ -102,15 +102,15 @@ class myOpener(sublime_plugin.EventListener):
 			
 
 
-			# if(temp_dir in savedictionary):
-			# 	savedictionary[temp_dir] += 1
-			# 	sublime.message_dialog(str(savedictionary[temp_dir]))
-			# 	sublime.message_dialog(str(settings.get("x_savespush")))
-			# 	if(savedictionary[temp_dir] == settings.get("x_savespush")):
-			# 		savedictionary[temp_dir] = 0
-			# 		push_repo()
-			# else:
-			# 	 savedictionary[temp_dir] = 1
+			if(temp_dir in savedictionary):
+				savedictionary[temp_dir] += 1
+				sublime.message_dialog(str(savedictionary[temp_dir]))
+				sublime.message_dialog(str(settings.get("x_savespush")))
+				if(savedictionary[temp_dir] == settings.get("x_savespush")):
+					savedictionary[temp_dir] = 0
+					push_repo()
+			else:
+				 savedictionary[temp_dir] = 1
 
 
 			# global number_saves_before_push
